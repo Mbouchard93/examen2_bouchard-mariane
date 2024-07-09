0e60917606714b5e636f4f8fb9330803494f58a5
Mbouchard93 <mariiane.bouchard12@gmail.com>
2024-07-09 12:11:45 -0400

    examen

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -6,10 +6,11 @@ import '../style/main.css'
  * @param {string} pxLg 
  */
 class Card {
-    constructor(index, pxSm, pxLg) {
+    constructor(index, pxSm, pxLg, nb) {
         this.index = index;
         this.pxSm = pxSm;
         this.pxLg = pxLg;
+        this.nb = nb;
     }
     /**
      * 
@@ -20,10 +21,12 @@ class Card {
         const index = document.createElement('span');
         const pxSm = document.createElement('span');
         const pxLg = document.createElement('span');
+        const nb = document.createElement('span')
 
         index.textContent = this.index;
         pxSm.textContent = this.pxSm;
         pxLg.textContent = this.pxLg;
+        nb.textContent = this.nb;
         div.className = 'bg-black w-[200px] h-[200px] lg:w-[300px] lg:h-[300px] rounded-lg text-white flex items-center justify-center flex-col card text-md font-semibold'
         pxSm.className = 'lg:hidden'
         pxLg.className = 'hidden lg:block'
@@ -31,7 +34,7 @@ class Card {
         div.appendChild(index);
         div.appendChild(pxSm);
         div.appendChild(pxLg);
-
+        div.appendChild(nb)
         return div
     }
 }
@@ -41,15 +44,15 @@ class Card {
  * @type {Card[]}
  */
 const cards = [
-    new Card(1, '200px / 200px', '300px / 300px'),
-    new Card(2, '200px / 200px', '300px / 300px'),
-    new Card(3, '200px / 200px', '300px / 300px'),
-    new Card(4, '200px / 200px', '300px / 300px'),
-    new Card(5, '200px / 200px', '300px / 300px'),
-    new Card(6, '200px / 200px', '300px / 300px'),
-    new Card(7, '200px / 200px', '300px / 300px'),
-    new Card(8, '200px / 200px', '300px / 300px'),
-    new Card(9, '200px / 200px', '300px / 300px'),
+    new Card(1, '200px / 200px', '300px / 300px',1),
+    new Card(2, '200px / 200px', '300px / 300px',2),
+    new Card(3, '200px / 200px', '300px / 300px',3),
+    new Card(4, '200px / 200px', '300px / 300px',4),
+    new Card(5, '200px / 200px', '300px / 300px',5),
+    new Card(6, '200px / 200px', '300px / 300px',6),
+    new Card(7, '200px / 200px', '300px / 300px',7),
+    new Card(8, '200px / 200px', '300px / 300px',8),
+    new Card(9, '200px / 200px', '300px / 300px',9),
 ]
 
 /**
@@ -88,4 +91,12 @@ btn.addEventListener('click', () => {
         cardIndex.classList.toggle('bg-pink-500')
     });
 
+})
+
+const btn2 = document.querySelector('.btn2')
+
+btn2.addEventListener('click', ()=> {
+    cardsIndex.forEach(cardIndex => {
+        cardIndex.classList.toggle('hidden');
+    })
 })
